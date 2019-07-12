bed029d56084e4b65ba84d1f23a459d0f07a4e2c
Sean Carver <seancarverphd@gmail.com>
2019-07-12 17:49:51 -0400

    Adding test_consistency.py

---
+++ etl/test_consistency.py
@@ -0,0 +1,25 @@
+import json
+import dictdiffer
+
+with open('../data/2005_codes.json') as json_file:
+    data_2005 = json.load(json_file)
+with open('../data/2006_codes.json') as json_file:
+    data_2006 = json.load(json_file)
+with open('../data/2007_codes.json') as json_file:
+    data_2007 = json.load(json_file)
+with open('../data/2008_codes.json') as json_file:
+    data_2008 = json.load(json_file)
+with open('../data/2009_codes.json') as json_file:
+    data_2009 = json.load(json_file)
+with open('../data/2010_codes.json') as json_file:
+    data_2010 = json.load(json_file)
+with open('../data/2011_codes.json') as json_file:
+    data_2011 = json.load(json_file)
+with open('../data/2012_codes.json') as json_file:
+    data_2012 = json.load(json_file)
+with open('../data/2013_codes.json') as json_file:
+    data_2013 = json.load(json_file)
+with open('../data/2014_codes.json') as json_file:
+    data_2014 = json.load(json_file)
+with open('../data/2015_codes.json') as json_file:
+    data_2015 = json.load(json_file)
